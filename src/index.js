da58282febdcdd964aa1b0a95b3be72021735bbe
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2024-01-05 12:30:43 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { StateProvider } from "./StateProvider";
+import Reducer, { initialState } from "./Reducer";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <StateProvider initialState={initialState} reducer={Reducer}>
+      <App />
+    </StateProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
